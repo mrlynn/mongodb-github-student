d45eb665c46b809e0dd34b052949f7e0d7d082f7
Michael Lynn <merlynn@gmail.com>
2020-01-01 18:13:34 -0500

    Deploying to Heroku

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const flash = require('connect-flash');
 const helmet = require('helmet');
 var partials = require('express-partials');
 var fileUpload = require('express-fileupload');
+var http = require('http');
+var debug = require('debug')('server:server');
+
 
 // Authentication
 const session = require('express-session');
@@ -114,3 +117,77 @@ app.use(function(req, res, next) {
 //   res.render('error');
 // });
 module.exports = app;
+
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
--- routes/index.js
+++ routes/index.js
@@ -67,119 +67,13 @@ router.get("/admin", isAdmin(), function(req, res, next) {
   });
 });
 
-// router.get("/auth/github/callback", (req, res, next) => {
-//   const { query } = req;
-//   const { code } = query;
-//   console.log("Got here");
-//   if (!code) {
-//     res.send({
-//       success: false,
-//       message: "Error: code not received"
-//     });
-//   }
-//   const data = {
-//     client_id: process.env.GITHUB_CLIENT_ID,
-//     client_secret: process.env.GITHUB_CLIENT_SECRET,
-//     code: code,
-//     scope: "read:user"
-//   };
-//   console.log("STEP 4 Continued - send data " + JSON.stringify(data));
-//   request
-//     .post("https://github.com/login/oauth/access_token")
-//     .send({
-//       client_id: process.env.GITHUB_CLIENT_ID,
-//       client_secret: process.env.GITHUB_CLIENT_SECRET,
-//       code: code,
-//       scope: "read:user"
-//     })
-//     .set("Accept", "application/json")
-//     .set("Authorization", "token " + code)
-//     .then(response => {
-//       console.log(
-//         "Complete Response from Step 5 = " + JSON.stringify(response.body)
-//       );
-//       const token = response.body.access_token;
-//       console.log(
-//         "STEP 5 - Receive Token -  Got a response from step 2 including a bearer token: " +
-//           token +
-//           " - done"
-//       );
-//       if (token.length <= 10) {
-//         res.send({
-//           success: false,
-//           message: "Invalid token received"
-//         });
-//       }
-//       request
-//         .get("https://education.github.com/api/user")
-//         .set("Accept", "application/json")
-//         .set("Authorization", "token " + token)
-//         .then(response => {
-//           console.log(
-//             "STEP 6 success - Send token back to api (https://education.github.com/api/user) to get user details -  Next, sent token back to github to get user data and got this response: " +
-//               JSON.stringify(response.body)
-//           );
-//           console.log(
-//             "Is this person a student?" + JSON.stringify(response.body)
-//           );
-//           if (response.body.student == false) {
-//             // res.redirect("http://localhost:3000/notstudent"); // redirect to a page letting the student know about signing up for the student pack
-//             // following is just for testing because we don't have an actual student approved to test with yet.
-//             // Let's try to get user details so we can record for fulfillment.
-//             console.log("Step 7 - send a request to get user profile data.");
-//             console.log("GET https://api.github.com/user with token " + token);
-
-//             const AuthStr = "Bearer ".concat(token);
-//             axios
-//               .get("https://api.github.com/user", {
-//                 headers: { Authorization: AuthStr }
-//               })
-//               .then(response => {
-//                 // If request is good...
-//                 console.log(response.data);
-//                 var rtn = addUser(response.data, token);
-//               })
-//               .catch(error => {
-//                 console.log("error " + error);
-//               });
-//           } else {
-//             // student: true
-//             // Let's get and then record the student profile details
-//             const AuthStr = "Bearer ".concat(token);
-//             axios
-//               .get("https://api.github.com/user", {
-//                 headers: { Authorization: AuthStr }
-//               })
-//               .then(response => {
-//                 // If request is good...
-//                 console.log(response.data);
-//               })
-//               .catch(error => {
-//                 console.log("error " + error);
-//               });
-//           }
-//         })
-//         .catch(err => {
-//           console.log(
-//             "STEP 6 fail - Send token back to api (https://education.github.com/api/user) to get user details - Got error trying to get user data: " +
-//               JSON.stringify(err.message)
-//           );
-//         });
-//     })
-//     .catch(err => {
-//       console.log(
-//         "STEP 4 Send code back to get a bearer token - Got error trying to get token: " +
-//           JSON.stringify(err.message)
-//       );
-//     });
-// });
 function authenticationMiddleware() {
   return (req, res, next) => {
     console.log(
       `req.session.passport.user: ${JSON.stringify(req.session.passport)}`
     );
     if (req.isAuthenticated()) {
-      req.user = req.session.passport;
+      // req.user = req.session.passport;
       return next();
     }
     res.redirect("/");
